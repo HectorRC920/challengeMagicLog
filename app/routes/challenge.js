9618dd1189f32335eada87bed694c4b45d158aa3
hrubio <hectorrubio@grupopromessa.com>
2023-07-20 16:59:42 -0600

    advances

--- app/routes/challenge.js
+++ app/routes/challenge.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
-
-router.get('/', function (req, res,next){
-  return res.send('hola desde challenge controller');
+const axios = require('axios');
+router.get('/', async (req, res,next) => {
+  const response = await axios.get('https://coderbyte.com/api/challenges/json/age-counting')
+  const data = response.data;
+  //get how many items exist that have an age equal to 32
+  const items = data.data.split(', ');
+  // console.log(items);
+  const groupedItems = [];
+  for (let i = 0; i < items.length; i++) {
+    groupedItems[i] = [items[i], items[i++ + 1]]; 
+    
+  }
+  console.log(cleanItems);
+  return res.send(groupedItems);
 }); 
 
 module.exports = router;
